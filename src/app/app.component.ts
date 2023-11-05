59cae3ad09954942ff830023a0bae5a315f817cc
shibili-chaliyam <shibili336@gmail.com>
2023-11-05 22:39:35 +0530

    next required validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { ToScrapData } from '../app/interfaces/to-scrap-data';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  scrapData:ToScrapData={
+    url:'',
+    keepInUrl:'',
+    pageLimit:0
+  }
+
+  onStart() {
+    alert("hello");
+  }
+
   title = 'WebScrapper.UI';
 }
---
+++ src/app/interfaces/to-scrap-data.ts
@@ -0,0 +1,5 @@
+export interface ToScrapData {
+    url:string,
+    keepInUrl:string,
+    pageLimit:number  
+}
